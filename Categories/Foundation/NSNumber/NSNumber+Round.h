cc88c43f921def2af2863ddbd1d0049006f4188d
lisong <741871959@qq.com>
2016-10-08 15:43:35 +0800

    add new

---
+++ Categories/Foundation/NSNumber/NSNumber+Round.h
@@ -0,0 +1,45 @@
+//
+//  NSNumber+Round.h
+//  Categories
+//
+//  Created by lisong on 15/12/21.
+//  Copyright © 2015年 haohaozhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSNumber (Round)
+
+/* 展示 */
+- (NSString*)toDisplayNumberWithDigit:(NSInteger)digit;
+- (NSString*)toDisplayPercentageWithDigit:(NSInteger)digit;
+
+/*　四舍五入 */
+/**
+ *  @brief  四舍五入
+ *
+ *  @param digit  限制最大位数
+ *
+ *  @return 结果
+ */
+- (NSNumber*)doRoundWithDigit:(NSUInteger)digit;
+
+/**
+ *  @brief  取上整
+ *
+ *  @param digit  限制最大位数
+ *
+ *  @return 结果
+ */
+- (NSNumber*)doCeilWithDigit:(NSUInteger)digit;
+
+/**
+ *  @brief  取下整
+ *
+ *  @param digit  限制最大位数
+ *
+ *  @return 结果
+ */
+- (NSNumber*)doFloorWithDigit:(NSUInteger)digit;
+
+@end
---
+++ Categories/UIKit/UITableView/UITableView+Common.h
@@ -0,0 +1,20 @@
+//
+//  UITableView+Common.h
+//  Coding_iOS
+//
+//  Created by 王 原闯 on 14-8-5.
+//  Copyright (c) 2014年 Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@interface UITableView (Common)
+
+- (void)addRadiusforCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath;
+
+- (void)addLineforPlainCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath withLeftSpace:(CGFloat)leftSpace;
+
+- (void)addLineforPlainCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath withLeftSpace:(CGFloat)leftSpace hasSectionLine:(BOOL)hasSectionLine;
+
+- (void)addLineforPlainCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath withLeftSpace:(CGFloat)leftSpace withLineColor:(UIColor*)lineColor;
+
+@end
