9a1d9213d1c0a27666c9d117b26a818d8be7ba2d
lisong <741871959@qq.com>
2016-10-14 15:53:21 +0800

    add new

---
+++ Categories/UIKit/UIButton/UIButton+ClickEdgeInsets.h
@@ -0,0 +1,17 @@
+//
+//  UIButton+ClickEdgeInsets.h
+//  Categories
+//
+//  Created by lisong on 2016/10/14.
+//  Copyright © 2016年 lisong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton (ClickEdgeInsets)
+
+@property (nonatomic, assign) CGFloat clickInset;
+
+@property (nonatomic, assign) UIEdgeInsets clickEdgeInsets;
+
+@end
---
+++ Categories/UIKit/UIControl/UIControl+Delay.h
@@ -0,0 +1,15 @@
+//
+//  UIControl+Delay.h
+//  Categories
+//
+//  Created by lisong on 2016/10/14.
+//  Copyright © 2016年 lisong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIControl (Delay)
+
+@property (assign, nonatomic) NSTimeInterval acceptEventInterval;
+
+@end
